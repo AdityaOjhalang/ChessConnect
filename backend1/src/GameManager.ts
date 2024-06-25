ae779934b6c5cfe853af06f3f4211f9757052522
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-06-25 15:33:50 -0700

    bug fixes in GameManager.ts

---
+++ backend1/src/GameManager.ts
@@ -0,0 +1,52 @@
+import { WebSocket } from "ws";
+import { INIT_GAME } from "./messages";
+import { Game } from "./Game";
+
+
+export class GameManager {
+	private games: Game[] = [];
+	private users: WebSocket[] = [];
+	private pendingUser: WebSocket | null; // This might be meant to be WebSocket[] if you intend to manage multiple pending users
+
+	constructor() {
+		this.games = [];
+		this.pendingUser = null;
+		this.users = [];
+	}
+
+	addUser(socket: WebSocket) {
+		this.users.push(socket);
+		this.addHandler(socket); // Assuming addHandler is a method you've implemented elsewhere
+	}
+
+	removeUser(socket: WebSocket) {
+		this.users = this.users.filter(user => user !== socket);
+		// Stop the game here (Implementation depends on how you manage games)
+	}
+
+	private addHandler(socket: WebSocket) {
+		socket.on("message", (data: string) => {
+			const message = JSON.parse(data);
+			if (message.type === INIT_GAME) {
+				if (this.pendingUser) {
+					//start the game
+					const game = new Game(this.pendingUser, socket);
+					this.games.push(game);
+					this.pendingUser = null;
+
+				}
+				else {
+					this.pendingUser = socket;
+				}
+			}
+			if (message.type === "move") {
+				const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+				if (game) {
+					game.makeMove(socket, message.move);
+				}
+			}
+		});
+	}
+
+	// Additional methods (addHandler, createGame, joinGame) need to be defined
+}
